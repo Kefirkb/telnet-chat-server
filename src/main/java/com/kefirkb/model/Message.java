63e4ea47a9b5f341f68ad478d936e6b9aaae33ad
Sergey Nikiforov <snikiforov@devexperts.com>
2018-06-25 15:52:03 +0300

    [4] - cleanup code

--- src/main/java/com/kefirkb/model/Message.java
+++ src/main/java/com/kefirkb/model/Message.java
@@ -6,8 +6,19 @@ import javax.annotation.Nonnull;
  * Simple message interface
  */
 public interface Message {
+	/**
+	 * Method returns text of message
+	 *
+	 * @return text of message
+	 */
 	@Nonnull
 	String getMessageText();
+
+	/**
+	 * Method returns sender name
+	 *
+	 * @return sender name
+	 */
 	@Nonnull
 	String getSenderName();
 }
